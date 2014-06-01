2b105c47f1960f518e871f83fcc77979547619f1
Alex Conway <alexconway808@gmail.com>
2014-05-31 16:38:37 -1000

    total score added in to javascript

--- prompt_quiz.js
+++ prompt_quiz.js
@@ -1,39 +1,42 @@
 (function(){
-var answer = prompt ("What is the first North Shore surf spot?")
+var answer = prompt ("What is the first North Shore surf spot?");
+var total_score = 0;
 
-if console.log (answer)== "Haleiwa" {
-	1
+if (answer == "Haleiwa") {
+	total_score += 1;
 } else {
-	0
+	total_score == 0;
 }
 
 var answer = prompt ("What is the second North Shore surf spot?")
-if console.log (anwer) == "Laniakea" {
+if (console.log (anwer) == "Laniakea") {
 	1
 } else {
 	0
 }
 
 var answer = prompt ("What is the third North Shore surf spot?")
-if console.log (answer) == "Waiamea Bay" {
+if (console.log (answer) == "Waiamea Bay") {
 	1
 } else {
 	0
 }
 
 var answer = prompt ("What is the forth North Shore surf spot?")
-if console.log (answer) == "Pipeline" {
+if (console.log (answer) == "Pipeline") {
 	1
 } else {
 	0
 }
 
 var answer = prompt ("What is the fifth North Shore surf spot?")
-if console.log (answer) == "Sunset" {
+if (console.log (answer) == "Sunset") {
 	1
 } else {
 	0
 }
+
+alert(total_score);
 
 })();
 
