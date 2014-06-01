fc29373976ce0c35513beced508dc7acccdf77c9
Alex Conway <alexconway808@gmail.com>
2014-05-31 16:29:36 -1000

    var answer and if else statements

--- prompt_quiz.js
+++ prompt_quiz.js
@@ -1,3 +1,41 @@
 (function(){
+var answer = prompt ("What is the first North Shore surf spot?")
+
+if console.log (answer)== "Haleiwa" {
+	1
+} else {
+	0
+}
+
+var answer = prompt ("What is the second North Shore surf spot?")
+if console.log (anwer) == "Laniakea" {
+	1
+} else {
+	0
+}
+
+var answer = prompt ("What is the third North Shore surf spot?")
+if console.log (answer) == "Waiamea Bay" {
+	1
+} else {
+	0
+}
+
+var answer = prompt ("What is the forth North Shore surf spot?")
+if console.log (answer) == "Pipeline" {
+	1
+} else {
+	0
+}
+
+var answer = prompt ("What is the fifth North Shore surf spot?")
+if console.log (answer) == "Sunset" {
+	1
+} else {
+	0
+}
 
 })();
+
+
+
