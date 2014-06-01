2eca52beebcd4b621c3b8cf671a9c2e2633ef999
Alex Conway <alexconway808@gmail.com>
2014-05-31 16:43:25 -1000

    first draft of quiz with javascript

--- prompt_quiz.js
+++ prompt_quiz.js
@@ -1,39 +1,44 @@
 (function(){
+
+
 var answer = prompt ("What is the first North Shore surf spot?");
 var total_score = 0;
-
 if (answer == "Haleiwa") {
 	total_score += 1;
 } else {
 	total_score == 0;
 }
 
-var answer = prompt ("What is the second North Shore surf spot?")
-if (console.log (anwer) == "Laniakea") {
-	1
+
+var answer = prompt ("What is the second North Shore surf spot?");
+if (anwer == "Laniakea") {
+	total_score += 1;
 } else {
-	0
+	total_score == 0;
 }
 
-var answer = prompt ("What is the third North Shore surf spot?")
-if (console.log (answer) == "Waiamea Bay") {
-	1
+
+var answer = prompt ("What is the third North Shore surf spot?");
+if (answer == "Waiamea Bay") {
+	total_score += 1;
 } else {
-	0
+	total_score = 0;
 }
 
-var answer = prompt ("What is the forth North Shore surf spot?")
-if (console.log (answer) == "Pipeline") {
-	1
+
+var answer = prompt ("What is the forth North Shore surf spot?");
+if (answer == "Pipeline") {
+	total_score += 1;
 } else {
-	0
+	total_score = 0;
 }
 
-var answer = prompt ("What is the fifth North Shore surf spot?")
-if (console.log (answer) == "Sunset") {
-	1
+
+var answer = prompt ("What is the fifth North Shore surf spot?");
+if (answer == "Sunset") {
+	total_score += 1;
 } else {
-	0
+	total_score = 0;
 }
 
 alert(total_score);
